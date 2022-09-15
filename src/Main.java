01c8a3b22ac7d338ac499d509d610d4f0e20a913
SofiaMonteiro27 <scl.monteiro@campus.fct.unl.pt>
2022-09-15 17:27:22 +0100

    Teste de mensagem de commit.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Sofia Monteiro
+ *
+ * Projeto de teste do Git
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Escrutinar seu programa desgracado");
+    }
+}
