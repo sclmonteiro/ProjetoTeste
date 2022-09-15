17aae1065e76ecace36143bbac7335661148ac5a
SofiaMonteiro27 <scl.monteiro@campus.fct.unl.pt>
2022-09-15 17:38:53 +0100

    Acrescentado ainda mais escrutinio ao anterior.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Escrutinar seu programa desgracado");
+        System.out.println("Escrutinar ainda mais do que o costume, seu programa desgracado");
     }
 }
